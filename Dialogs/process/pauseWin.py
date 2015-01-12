b644f04d9dd38b88652f1b7dc81fcb4d444a946b
Carlos Feras Hernandez <c4rlos.ferra5@gmail.com>
2015-01-12 11:24:45 -0500

    Version 4

--- Dialogs/process/pauseWin.py
+++ Dialogs/process/pauseWin.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*- 
 
 #~ Copyright (C) 2014 Carlos Manuel Ferras Hernandez <c4rlos.ferra5@gmail.com>
-#~ This file is part of LF02_package.
+#~ This file is part of Secuence-ToolKit.
 
-#~ LF02_package is free software: you can redistribute it and/or modify
+#~ Secuence-ToolKit is free software: you can redistribute it and/or modify
 #~ it under the terms of the GNU General Public License as published by
 #~ the Free Software Foundation, either version 3 of the License, or
 #~ (at your option) any later version.
 
-#~ LF02_package is distributed in the hope that it will be useful,
+#~ Secuence-ToolKit is distributed in the hope that it will be useful,
 #~ but WITHOUT ANY WARRANTY; without even the implied warranty of
 #~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #~ GNU General Public License for more details.
 
 #~ You should have received a copy of the GNU General Public License
-#~ along with LF02_package.  If not, see <http://www.gnu.org/licenses/>.
+#~ along with Secuence-ToolKit.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 from UI.process import Pause
 from Dialogs.process import infoWin
